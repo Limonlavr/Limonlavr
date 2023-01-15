d991fad19047ce91d859ea5893bf8c9591cbd33a
Komarovdb <hellosser788@gmail.com>
2023-01-15 13:11:33 +0700

    Я люблю пробрагромирование

---
+++ lesson 14.py
@@ -0,0 +1,72 @@
+# s = {"A", "Диван", "Дед"}
+# print(s)
+#
+# def hello_world(): # обьявление функции
+#     print("hello_world")
+#
+# #____________________________
+#
+# hello_world()  # вызов функции
+#
+
+# def plus(chislo1, chislo2:int) -> int:
+#     """
+#
+#
+# #     :param chislo1:
+# #     :param chislo2:
+# #     :return:
+# #     """
+#     answer = chislo1 + chislo2
+#     return answer # что вернёт функцию
+# x = plus(54, 88)
+# print(x + 2)
+#
+# plus()
+
+# def is_sorted(liist_0ne):
+#     clone_l = sorted(liist_0ne)
+#     if liist_0ne == clone_l:
+#         return True
+#
+# l = [9, 5, 6, 7, 8]
+
+#
+# if is_sorted(l)
+#     print("ринтанул")
+# else:
+#     print("false")
+
+
+
+
+
+# Вторая задач
+# def find_longest(slova1):
+#     shifrt=[]
+#     for i in slova1:
+#         shifrt.append(len(i)) # добавление элемента в список
+#     print(shifrt)
+#     imd288= shifrt.idex(max(shifrt))
+#     return slova1[imd228],shifrt[imd288]
+# slova=["lol", "lox", "utut","erty", "egg"]
+#
+# print(find_longest(slova))
+
+
+
+# Задач 3
+
+# def min_max(qwe):
+#     mini=qwe[0]
+#     maxi=qwe[0]
+#     for i in qwe:
+#         if i > maxi: #  если найден новый максимум
+#             maxi=i #  перезапись максимума
+#         elif i < mini:
+#             mini = i
+#     return mini,maxi
+# qwe = [4,3,5,6,7]
+# print("Минимальное:",min_max(qwe)[0],"аксимальное:"min_)
+#
+# # print("Минимальное:",min_max)
