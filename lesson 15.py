46b7fa51af18f54b465adf017695ec63ae3480c3
movavi_school <pasha.00x.ll.xx.yt@gmail.com>
2023-01-22 13:11:22 +0700

    aaaaa

---
+++ lesson 15.py
@@ -0,0 +1,87 @@
+# #лямда(Lambda)
+# x = 10
+# y = 5
+# l1 = lambda x, y: x + y + 1
+# print(l1(x , y))
+
+
+# answer = "д"
+# result = lambda is_exit: True if is_exit == "Д" else False
+# print(result(answer))
+
+# l = []
+# for i in range(1, 5+1):
+#     l.append()
+# print(l)
+#
+# l2 = [i for i in range(1, 6)]
+#1 list comprehension всегда в []
+#2 for i in... обычный цикл -> сколбко элементов будкт в
+
+
+# Задачя 1
+# c2f = lambda c: 9/5 * c + 32
+# print(c2f(100))
+# f2c = lambda f: 5/9 * (f - 32)
+# print(f2c(82))
+#
+# c2k = lambda c: c + 273.15
+# print(c2k(30))
+#
+# k2c = lambda k: k - 273.15
+# print(k2c(450))
+#
+# f2k = lambda f: c2k(f2c(f))
+# print(f2k(203))
+
+
+
+# Задачя №2
+# from random import random
+# exit = lambda answer: True if answer == "д"
+#      ckolko = int(input("сколько:"))
+#      dies = [randint(1, 6)for i in range(ckolko)]
+#      print(dies)
+#      answer = input("Хотите выйти"
+#                     "ша учше()
+    # da = input("хотите выйти д/n")
+    # if da == "д":
+    #     print("уд бай")
+    #     x = 1
+    # elif da == "h":
+    #     x = 0
+    # else:
+    #     print("научились песать")
+
+# Задачя 3
+# from random import choice
+# chars = [list("АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"),
+#          list("абвгдеёжзийклмнопрстуфхцчшщъыьэюя"),
+#          list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+#          list("abcdefghijklmnopqrstuvwxyz"),
+#          list("1234567890")
+#         ]
+# vses =  (choice(chars(chars))) for i in range(3+3)
+# tvss = '0' .joinvesde ()
+# print(tvss)
+# if onvvodil in nepustooislovar
+# else:
+#     vses
+#     print
+#
+# адачя 4
+
+# a = 5
+# b = 2
+# otvet = lambda  a, b: b - a
+# otvet1 = lambda  a, b: a/b
+# otvet2 = lambda  a, b: b + a
+# otvet3 = lambda  a, b: b/a
+# otvet4 = lambda  a, b: b*a
+# otvet5 = lambda x,: if x < 0 else x
+# print(otvet5(5, 25))
+
+#
+# field = {"A": ["⬜", "⬜", "⬜"],
+#          "B": ["⬜", "⬜", "⬜"],
+#          "C": ["⬜", "⬜", "⬜"]}
