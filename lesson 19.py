07a9b2cd5b68b859b99ada9daabcda4c6d474822
movavi_school <andrejcepurnoj3@gmail.com>
2023-02-12 13:13:49 +0700

    я люблю негров

---
+++ lesson 19.py
@@ -0,0 +1,17 @@
+# def abvgedeika
+# reverse = c[::-1]
+# c = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+# c = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+# sms = input("Веди слово для шифрования :")
+# word=''
+# for i in sms:  #i = каждый символ
+#     ind = alphabetmaxi.index
+#     if i not in alphabetmaxi or alphabetmaxi:
+#         word = word + i
+#     letter=
+#     if i.isupper():
+#     else:
+#     ind=c.index(i)
+#     letter=reverse[ind]
+#     word=word+letter
+# print(word)
