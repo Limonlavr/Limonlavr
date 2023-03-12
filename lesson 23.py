3450131a1aa124f4bc8c5fd6356a6d7515554bb3
movavi_school <Kovalenkoaa2008@icloud.com>
2023-03-12 13:08:57 +0700

    негры топ

---
+++ lesson 23.py
@@ -0,0 +1,46 @@
+
+# a = open("file.json", "w", encoding="utf-8")
+# content = json.dump("Приффки", a,ensure_ascii=False)
+
+# a = open("file.json", "r", encoding="utf-8")
+# content = json.load(a)
+# print(content)
+# a.close()
+
+
+#Новая тема
+# class Person:
+#     def __init__(self, imya):  # магический метод для инициализации
+#         self.mishurik = imya
+#         self.status = "Учитель"
+#
+#     def __str__(self):
+#        return f"танки говно:{self.mishurik} {self.status}"
+#
+#     def common_method(self):
+#         print("убили негра убили неграа а а а а  ")
+
+#--------------------------------------------------
+
+# chelovek = Person("Ы")
+#
+# print(chelovek.mishurik)
+# print(chelovek)
+# print(chelovek)
+
+#Задаччя 1
+# class Iaga:
+#     def __init__(self, lof, yas, ag):
+#         self.name= lof
+#         self.surnama= yas
+#         self.age=ag
+#     def __str__(self):
+#         return f"имя{self.name},\nФамилия:{self.surnama},\nвозраст:{self.age}"
+#     def greet(self):
+#         return f"Привет"
+# tank = Iaga("niger", "Hahol", 65)
+# print(tank.name, tank.surnama, tank.age)
+# print(tank)
+# print(tank.greet())
+
+for i in range(0, 5, 2)
