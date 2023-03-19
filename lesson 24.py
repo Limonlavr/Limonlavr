0ef9bbfc41fc229241843d00e7d57ab8adbf8874
movavi_school <Kovalenkoaa2008@icloud.com>
2023-03-19 13:06:37 +0700

    логика оф

---
+++ lesson 24.py
@@ -0,0 +1,52 @@
+#  class Mashina:
+#      def __init__(self, wheel:int, doors:int, pdk:bool):
+#          self.wheels = wheel #пример
+#          self.doors = doors
+#          self.pdk = pdk
+#          self.__probeg = 10_000
+#
+#      def go_out(self):
+#          if self.pdk == True:
+#            self.pdk = False
+#          return  "ядя Кля изгнан"
+#          else:
+#             return"Машина чиста.Его здесь нет."
+#      def obnulyai(self):
+#          self.__probeg = 0
+#   nissan = Mashina(wheel=8, doors=8, pdk=True)
+# print(nissan.go_out())
+# print(nissan.pdk)
+# nissan.pdk = True
+#
+# print(nissan._probeg)
+# nissan._probeg = 500 #приват можнл менять?Нона самом деле это паблик
+# nissan._probeg
+#Задачя---------------------------------------------------------------
+# import string
+# class Alphadet:
+#     def __init__(self, lang, letters):
+#         self.iang = "eng"
+#         self.letters = letters
+#
+#     def print(self):
+#         return self.letters
+#
+#     def nums_kolvo(self):
+#         return len(self.letters)
+#
+# alpha = Alphadet("end", "abcdefghijklmnopqrstuvwxyz")
+# print(alpha.print())
+# print(alpha.nums_kolvo())
+#----------------------------------------------------------------
+
+# import datetime
+# class Chacc:
+#
+#     def __init__(self):
+#         self.ntime = datetime.datetime.now()strftime("%H:%M:%S")
+#
+# laboe = Chasi()
+# print(luboe.ntime)
+# def addH
+
+
