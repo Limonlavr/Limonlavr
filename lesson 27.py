71a3251cf96b305d7f19336c4a7d0cb12cb39076
movavi_school <alexomger@mail.ru>
2023-04-16 13:14:09 +0700

    найс

---
+++ lesson 27.py
@@ -0,0 +1,133 @@
+# from tkinter import *
+#
+# def hell_o(event):
+# # print("Ты гей ха ха хa")
+#
+#     message = ent.get()
+#     ent.delete(0, END)
+#     print(message)
+#
+#     message2 = txt.get(0.0, END)
+#     print(message2)
+#
+# root = Tk()  # Создание
+# root.title("Моя первая стадия дигродирования")
+# root.geometry("400x500")
+# root['bg'] = "wheat"
+# lab = Label(root, text="С пасхой （⊙ｏ⊙）")
+# lab["background"] = "red"
+# lab["background"] = "#ab54cd"# Передний цвет
+# lab["foreground"] = "silver"
+#
+# lab.pack() #pack() - размещение элемента
+# lab["font"] = 20 # 1 значение - размер
+# lab["font"] = "Arial 30 italic"
+# lab["font"] = ("Times New Roman", 20, "bold", "italic", "underline", "overstrike")
+#
+#
+# btn = Button(root, text="Кто тыкнет гей", font=20,
+#              width=20,
+#              height=10,
+#              bd=15,)
+#              # command=hell_o# Button - кнопка
+# btn.pack()
+# btn.bind("<Button-1>", hell_o)
+# #Button-3
+#
+# ent = Entry(root, bd=10, font=20, width=15)
+# ent.pack()
+#
+# txt = Text(root, width=20, height=5)
+# txt.pack()
+
+# root.mainloop() # Открытие
+
+#Задачя1
+# from tkinter import *
+#
+# window = Tk()
+# window.geometry("250x250")
+# lab = Label(window, text="galaxy", font=20)
+# lab.pack()
+# ent=Entry(window, font=20, width=20)
+# ent.pack()
+#
+# btn1=Button(window, width=30, bg="red")
+# btn1=Button(window, width=30, bg="red")
+# btn1=Button(window, width=30, bg="red")
+# btn1=Button(window, width=30, bg="red")
+# btn1=Button(window, width=30, bg="red")
+# btn1=Button(window, width=30, bg="red")
+# btn1=Button(window, width=30, bg="red")
+# btn1=Button(window, width=30, bg="red")
+# btn1.pack()
+#
+#
+# window.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Задачя 2
+# root = Tk()
+# root.geometru("500x500")
+# root
+#
+# lab = Label(root, text="Ваш адрес", bg="#FFE4C4")
+# lab.pack()
+#
+# lab = Label(root, text="Коментарий: ")
+# lab.pack()
+#
+# txt = Text(root, width=5, bd=5)
+# txt.pack()
+#
+# bin = Button(root,text="отправить", bd=5, bg"#JJA11IJ")
+# bin
+
+
+
+
+def itahli():
+    lab["font"] = r += jgj
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
